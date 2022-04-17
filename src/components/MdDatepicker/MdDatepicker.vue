f859ad45f3fab54e61fc6eac56de4e328c7d593d
Joshua Tam <joshua.tam@schoolteam.com.hk>
2022-04-17 18:11:20 +0800

    Disable keyboard on mobile device for datepicker

--- src/components/MdDatepicker/MdDatepicker.vue
+++ src/components/MdDatepicker/MdDatepicker.vue
@@ -1,7 +1,7 @@
 <template>
   <md-field :class="['md-datepicker', { 'md-native': !this.mdOverrideNative }]" :md-clearable="mdClearable" @md-clear="onClear">
     <md-date-icon class="md-date-icon" @click.native="toggleDialog" />
-    <md-input :type="type" ref="input" v-model="inputDate" @focus.native="onFocus" :pattern="pattern" :disabled="disabled" :readonly="readonly" :required="required" />
+    <md-input :type="type" ref="input" v-model="inputDate" @focus.native="onFocus" :pattern="pattern" :disabled="disabled" :readonly="readonly" :required="required" inputmode="none" />
 
     <slot />
 
